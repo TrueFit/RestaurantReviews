2aca9671b808945207ac454711a3ebb025be03b4
Jerry Bachmann <bachmann.jerry@gmail.com>
2016-06-23 22:39:35 -0400

    Adding base test file, won't be using for this test.

---
+++ Truefit.Users.Tests/UserServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Truefit.Users.Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private IUserService _userService;
+        [SetUp]
+        public void Setup()
+        {
+            this._userService = new UserService();
+        }
+    }
+}
