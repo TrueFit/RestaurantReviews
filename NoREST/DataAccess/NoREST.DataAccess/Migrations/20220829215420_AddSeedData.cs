6798391e6d96788f1c4b4dfd41a3774401f70d81
chris.rune <gtrabbit@gmail.com>
2022-08-29 17:54:40 -0400

    add seed data to migrations

---
+++ NoREST/DataAccess/NoREST.DataAccess/Migrations/20220829215420_AddSeedData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoREST.DataAccess.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedOn", "IdentityProviderId", "IsAdmin", "IsSystemUser", "LastModifiedOn", "UserName" },
+                values: new object[] { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "5qg4one8ve2o7024st8dks8ejq", true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "NoRESTApi" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedOn", "IdentityProviderId", "IsAdmin", "IsSystemUser", "LastModifiedOn", "UserName" },
+                values: new object[] { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "b9c45965-95d5-4d45-a580-bafeba254ad0", true, false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "chris.rune" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- NoREST/DataAccess/NoREST.DataAccess/NoRESTContext.cs
+++ NoREST/DataAccess/NoREST.DataAccess/NoRESTContext.cs
@@ -15,31 +15,6 @@ namespace NoREST.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
-
-            //modelBuilder.Entity<Restaurant>()
-            //    .HasOne(r => r.CreatedBy).WithMany(u => u.Restaurants)
-            //    .IsRequired(false)
-            //    .HasForeignKey(r => r.CreatedById)
-            //    .IsRequired(false)
-            //    .OnDelete(DeleteBehavior.ClientSetNull);
-
-            //modelBuilder.Entity<Review>()
-            //    .HasOne(r => r.CreatedBy).WithMany(u => u.Reviews).HasPrincipalKey(u => u.Id)
-            //    .IsRequired(false)
-            //    .HasForeignKey(u => u.CreatedById)
-            //    .OnDelete(DeleteBehavior.ClientSetNull);
-            //modelBuilder.Entity<Review>()
-            //    .HasOne(r => r.Restaurant).WithMany(r => r.Reviews).HasPrincipalKey(r => r.Id)
-            //    .IsRequired(false)
-            //    .HasForeignKey(r => r.RestaurantId)
-            //    .OnDelete(DeleteBehavior.ClientSetNull);
-            //modelBuilder.Entity<Review>()
-            //    .HasKey(r => r.Id);
-
-            //modelBuilder.Entity<User>()
-            //    .HasKey(u => u.Id);
-
             modelBuilder.Entity<UserRestaurantBan>()
                 .HasKey(nameof(UserRestaurantBan.RestaurantId), nameof(UserRestaurantBan.UserId));
             modelBuilder.Entity<UserRestaurantBan>()
@@ -51,8 +26,12 @@ namespace NoREST.DataAccess
                 .HasForeignKey(b => b.RestaurantId)
                 .OnDelete(DeleteBehavior.ClientSetNull);
 
-            // new User { Id = 1, IdentityProviderId = "5qg4one8ve2o7024st8dks8ejq", UserName = "NoRESTApi", IsAdmin = true, IsSystemUser = true },
-            // new User { Id = 2, IdentityProviderId = "b9c45965-95d5-4d45-a580-bafeba254ad0", UserName = "chris.rune", IsAdmin = true, IsSystemUser = false }
+            modelBuilder.Entity<User>()
+                .HasData(
+                     new User { Id = 1, IdentityProviderId = "5qg4one8ve2o7024st8dks8ejq", UserName = "NoRESTApi", IsAdmin = true, IsSystemUser = true },
+                     new User { Id = 2, IdentityProviderId = "b9c45965-95d5-4d45-a580-bafeba254ad0", UserName = "chris.rune", IsAdmin = true, IsSystemUser = false }
+                );
+
         }
     }
 }
